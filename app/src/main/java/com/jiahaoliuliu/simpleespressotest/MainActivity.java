e4fc432517992caa2fdc7a01f4e4e02c064d2ea3
Jiahao Liu <jiahaoliuliu@gmail.com>
2016-10-09 15:21:28 +0400

    Created simple tests activities list

--- app/src/main/java/com/jiahaoliuliu/simpleespressotest/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/simpleespressotest/MainActivity.java
@@ -2,12 +2,51 @@ package com.jiahaoliuliu.simpleespressotest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    enum TestActivity {
+        SIMPLE_BUTTON_ACTIVITY(R.string.simple_button_activity, SimpleButtonActivity.class)
+        ;
+
+        private int mActivityTitleId;
+        private Class mActivityClass;
+
+        public int getActivityTitleId() {
+            return mActivityTitleId;
+        }
+
+        public Class getActivityClass() {
+            return mActivityClass;
+        }
+
+        TestActivity(int activityTitleId, Class activityClass) {
+            this.mActivityTitleId = activityTitleId;
+            this.mActivityClass = activityClass;
+        }
+    }
+
+    private RecyclerView mTestsRecyclerView;
+    private RecyclerView.LayoutManager mTestsLayoutManager;
+    private TestsListAdapter mTestsListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Link the views
+        mTestsRecyclerView = (RecyclerView) findViewById(R.id.tests_recycler_view);
+        mTestsRecyclerView.setHasFixedSize(true);
+
+        // Set the linear manager
+        mTestsLayoutManager = new LinearLayoutManager(this);
+        mTestsRecyclerView.setLayoutManager(mTestsLayoutManager);
+
+        // Set the adapter
+        mTestsListAdapter = new TestsListAdapter(this, TestActivity.values());
+        mTestsRecyclerView.setAdapter(mTestsListAdapter);
     }
 }
---
+++ app/src/main/java/com/jiahaoliuliu/simpleespressotest/SimpleButtonActivity.java
@@ -0,0 +1,18 @@
+package com.jiahaoliuliu.simpleespressotest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jiahao on 09/10/16.
+ */
+
+public class SimpleButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_simple_button);
+    }
+}
