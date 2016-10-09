d5ec8f783326bfeaa5e2a22b65646ece8ad89f45
Jiahao Liu <jiahaoliuliu@gmail.com>
2016-10-09 19:19:04 +0400

    Set the copy behaviour on the Edit text activity

--- app/src/main/java/com/jiahaoliuliu/simpleespressotest/SimpleEditTextActivity.java
+++ app/src/main/java/com/jiahaoliuliu/simpleespressotest/SimpleEditTextActivity.java
@@ -2,12 +2,35 @@ package com.jiahaoliuliu.simpleespressotest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.R.attr.button;
 
 public class SimpleEditTextActivity extends ChildActivity {
+
+    private EditText mSourceEditText;
+    private Button mCopyButton;
+    private TextView mDestinationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_edit_text);
+
+        // Link the views
+        mSourceEditText = (EditText) findViewById(R.id.source_edit_text);
+        mCopyButton = (Button) findViewById(R.id.copy_button);
+        mDestinationTextView = (TextView) findViewById(R.id.destination_text_view);
+
+        // Set the action
+        mCopyButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                mDestinationTextView.setText(mSourceEditText.getText().toString());
+            }
+        });
     }
 }
