b599e091084d5d6e58b69494949c516033a47211
Jiahao Liu <jiahaoliuliu@gmail.com>
2016-10-09 11:54:26 +0400

    Set simple button test

--- app/src/androidTest/java/com/jiahaoliuliu/simpleespressotest/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/jiahaoliuliu/simpleespressotest/ExampleInstrumentedTest.java
@@ -2,11 +2,19 @@ package com.jiahaoliuliu.simpleespressotest;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SmallTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -15,12 +23,26 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@SmallTest
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.jiahaoliuliu.simpleespressotest", appContext.getPackageName());
+    }
+
+    @Test
+    public void testOnButtonClicked() throws Exception {
+        onView(withId(R.id.simple_button))
+                .perform(click())
+                .check(matches(withText(R.string.simple_button_clicked)));
+
     }
 }
