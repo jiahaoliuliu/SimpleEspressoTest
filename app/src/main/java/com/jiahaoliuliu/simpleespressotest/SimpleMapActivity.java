6e84567bee9751931909a781532f08ea3a6852c4
Jiahao Liu <jiahaoliuliu@gmail.com>
2016-10-09 15:56:47 +0400

    Created a list of activities to test the Recycler view

---
+++ app/src/main/java/com/jiahaoliuliu/simpleespressotest/SimpleMapActivity.java
@@ -0,0 +1,13 @@
+package com.jiahaoliuliu.simpleespressotest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimpleMapActivity extends ChildActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_map);
+    }
+}
